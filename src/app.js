822ec87a16c907df0848fc2d6f5f7c2efdbf74f6
Ricardo Prada <prada-r.a@hotmail.com>
2022-02-07 19:26:39 -0500

    Handlebars (Template Engine)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
-//@ts-check
+// @ts-nocheck
 
+import path from "path";
 import express from "express";
+import {create} from "express-handlebars";
 import indexRoutes from "./routes/index.routes";
 
 const app = express();
 
+// settings
+app.set("views", path.join(__dirname, "views"));
+const exphbs = create({
+  extname: '.hbs',
+  layoutsDir: path.join(app.get("views"), "layouts"),
+  partialsDir: path.join(app.get("views"), "partials"),
+  defaultLayout:'main'
+});
+app.engine(".hbs", exphbs.engine);
+app.set("view engine", ".hbs");
+
+//Routes
 app.use(indexRoutes);
 
 export default app;
